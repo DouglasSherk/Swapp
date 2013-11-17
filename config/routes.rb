def54ff5899032e9065be805881ae3009116edc3
Ali Rushdan Tariq <alirtariq@gmail.com>
2013-11-17 01:45:29 -0500

    added search page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Swapp::Application.routes.draw do
     get '' => 'home#index', :as => 'templates'
     get 'recycle' => 'home#index', :as => 'templates_recycle'
     get 'pickups' => 'home#index', :as => 'templates_pickups'
+    get 'search' => 'home#index', :as => 'templates_search'
   end
 
   root 'home#index'
