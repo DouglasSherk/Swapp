d246095be31db8aab9f7c893db7e100eb0a8e6c1
DouglasSherk <github@sherk.me>
2013-11-16 23:05:02 -0500

    Fix app.js not loading

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   def index
-    render :layout => 'angular'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Swapp::Application.routes.draw do
   get 'pickups' => 'home#pickups'
 
-  get 'templates/:path.html' => 'app/templates#page', :constraints => { :path => /.+/ }, :as => 'templates_show'
+  get 'templates/:path.html' => 'templates#page', :constraints => { :path => /.+/ }, :as => 'templates_show'
 
   scope :templates do
     get '' => 'home#index', :as => 'templates'
