938e0fe712b8da4956bdf06ccb92c50fc22760f4
DouglasSherk <github@sherk.me>
2013-11-16 20:39:45 -0500

    Base Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swapp::Application.config.secret_key_base = 'fbff50f25da4f1bae4cb6641afbe709f824fd23812c6f8062903ec6eaf8ce87b93a84950aa0fb016dd43344eff0d59b5fa8c5b42aa3e12767af57fd39d6cc779'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Swapp::Application.config.session_store :cookie_store, key: '_swapp_session'
