14865e3160804d4ada0bc58ce94610ba9aaffd8b
Ali Rushdan Tariq <alirtariq@gmail.com>
2013-11-17 04:29:48 -0500

    added contact form

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Swapp::Application.routes.draw do
     get 'recycle' => 'home#index', :as => 'templates_recycle'
     get 'pickups' => 'home#index', :as => 'templates_pickups'
     get 'search' => 'home#index', :as => 'templates_search'
+    get 'contact' => 'home#index', :as => 'templates_contact'
   end
 
   root 'home#index'
